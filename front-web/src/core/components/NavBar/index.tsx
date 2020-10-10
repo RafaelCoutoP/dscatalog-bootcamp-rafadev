dc8da915edf4fae239da8b70fa6ded5759cfc197
Rafael Couto <Rafaelcouto@hotmail.com>
2020-10-09 21:49:07 -0300

    finaly Home

--- front-web/src/core/components/NavBar/index.tsx
+++ front-web/src/core/components/NavBar/index.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import './styles.scss';
+import { Link, NavLink } from 'react-router-dom';
 
 const NavBar = () => (
     //tag para dar mais semantica, tem a mesma função do div porem voltado a barra de navegação
     <nav className="row bg-primary main-nav">
         <div className="col-2">
-            <a href="link" className="nav-logo-text">
+            <Link to="/" className="nav-logo-text">
                 <h4>DS Catalog</h4>
-            </a>
-        </div> 
+            </Link>
+        </div>
         <div className="col-6 offset-2" /* offset-2 deu recur a esquerda em relação as 12 colunas*/>
             <ul className="main-menu">
                 <li>
-                    <a href="link" className="active">
+                    <NavLink to="/" activeClassName="active" exact>
                         HOME
-                  </a>
+                  </NavLink>
                 </li>
                 <li>
-                    <a href="link">
+                    <NavLink to="/catalog" activeClassName="active">
                         CÁTALOGO
-                  </a>
+                  </NavLink>
                 </li>
                 <li>
-                    <a href="link">
+                    <NavLink to="/admin" activeClassName="active">
                         ADMIN
-                  </a>
+                  </NavLink>
                 </li>
             </ul>
         </div>
