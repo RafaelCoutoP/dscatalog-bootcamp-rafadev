96ba8589a794111aabedafd5dfd89dfab28e5427
Rafael Couto <Rafaelcouto@hotmail.com>
2020-10-17 00:06:20 -0300

    Finaly pagination catalog

---
+++ front-web/src/core/components/Pagination/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './styles.scss';
+import { ReactComponent as ArrowIcon } from '../../assets/images/arrow.svg'
+import { generateList } from '../../utils/list';
+
+type Props ={
+    totalPages: number;
+    activePage: number;
+    onChange: (item:number) => void;
+}
+
+const Pagination = ({ totalPages, activePage, onChange }:Props) => {
+    const items = generateList(totalPages);
+    const previusClass = totalPages > 0 && activePage > 0 ? 'active-page' : 'inactive-page'; 
+    const nextClass = (activePage + 1) < totalPages ? 'active-page' : 'inactive-page' 
+     
+    return (
+        <div className="pagination-container">
+            <ArrowIcon 
+            className={`pagination-previos ${previusClass}`}  
+            onClick={() => onChange(activePage -1)}
+            />
+            {items.map(item =>(
+            <div
+            key={item} 
+            className={`pagination-item ${item === activePage ? 'active' : ''}`}
+            onClick={() => onChange(item)}
+            >
+              { item + 1 }
+            </div>    
+            ))}
+            <ArrowIcon  
+            className={`pagination-next ${nextClass}`}
+            onClick={() => onChange(activePage +1)}
+            />
+        </div>
+    );
+}
+
+export default Pagination;
--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -5,26 +5,29 @@ import { makeRequest } from '../../core/utils/request';
 import ProductCard from './components/ProductCard';
 import ProductCardLoader from './components/Loaders/ProductCardLoader';
 import './styles.scss';
+import Pagination from '../../core/components/Pagination';
 
 const Catalog = () => {
 
     const [productsResponse, setProductsResponse] = useState<ProductResponse>();
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const [activePage, setActivePage] = useState(0);
 
     // acessa o ciclo de vida do componente  
     useEffect(() => {
         const params = {
-            page: 0,
+            page: activePage,
             linesPerPage: 12
         }
-    // loading inicia como true 
+        setIsLoading(true)   
+        // loading inicia como true 
         makeRequest({ url: '/products', params })
             .then(response => setProductsResponse(response.data))
             .finally(() => {
                 setIsLoading(false);
             });
-    
-    }, []);
+
+    }, [activePage]);
 
     return (
         <div className="catalog-container">
@@ -40,8 +43,15 @@ const Catalog = () => {
                     ))
                 )}
             </div>
+            {productsResponse && (
+                <Pagination
+                    totalPages={productsResponse?.totalPages}
+                    activePage={activePage}
+                    onChange={page => setActivePage(page)}
+                />
+            )}
         </div>
-    );
+    )
 };
 
 export default Catalog;
