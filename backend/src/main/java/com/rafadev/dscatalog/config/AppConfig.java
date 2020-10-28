84bdef400b9edb4809b60819d6b17a2c35ce63c0
Rafael Couto <Rafaelcouto@hotmail.com>
2020-10-28 20:59:45 -0300

    External configurantion

--- backend/src/main/java/com/rafadev/dscatalog/config/AppConfig.java
+++ backend/src/main/java/com/rafadev/dscatalog/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.rafadev.dscatalog.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,10 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+	@Value("${jwt.secret}")
+	private String jwtSecret;
+	
+	
 	// componente gerenciado pelo SpringBoot por conta da anotação "Bean"
 	@Bean
 	public BCryptPasswordEncoder passwordEncoder() {
@@ -20,7 +25,7 @@ public class AppConfig {
 	@Bean
 	public JwtAccessTokenConverter accessTokenConverter() {
 		JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-		tokenConverter.setSigningKey("MY-JWT-SECRET");
+		tokenConverter.setSigningKey(jwtSecret);
 		return tokenConverter;
 	}
 
